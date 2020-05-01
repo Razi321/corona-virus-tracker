56bb56f37f3c9d876782722bf3902b522b029c65
unknown <razi melliti>
2020-05-01 01:35:57 +0100

    make it better

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import styles from './Footer.module.css';
+
+class Footer extends Component {
+  render() {
+    return (
+     
+          <div > 
+            <br/>
+     <br/>
+     <br/>
+            <footer className={styles.footer} >
+
+ 
+<div className="footer-copyright text-center py-3">© 2020 Copyright:
+  <a href="https://github.com/Razi321"> Razi Melliti</a>
+</div>
+
+
+</footer>
+          </div>
+
+
+    )
+  }
+}
+
+export default Footer
